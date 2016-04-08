214bbfe79faf4a3d8c6ee62fde3a1b7a3a2e948a
vladpolonskiy <opolo93@mail.ru>
2016-04-08 14:02:23 +0300

    create builder basic markup in template

--- templates/template.php
+++ templates/template.php
@@ -11,10 +11,9 @@ $pageId = $this->pageId;
 $form_action = 'editpost';
 add_thickbox();
 wp_enqueue_media(array('post' => $this->post_id));
-
 require_once(ABSPATH . 'wp-admin/admin-header.php');
 ?>
-
+<!--START PRELOADER -->
 <div id="loader-wrapper">
     <div id="loader">
         <div class="minutes-container"><div class="minutes"></div></div>
@@ -22,7 +21,74 @@ require_once(ABSPATH . 'wp-admin/admin-header.php');
     </div>
     <h1><?php _e('Please wait', 'qoob'); ?></h1>
 </div>
-
+<!--END PRELOADER -->
+<!--START BUILDER STRUCTURE-->
+<div id="builder">
+    <div id="builder-toolbar">
+        <div class="logo">
+            <div class="wrap-cube">
+                <div class="cube">
+                    <div class="front"></div>
+                    <div class="back"></div>
+                    <div class="top"></div>
+                    <div class="bottom"></div>
+                    <div class="left"></div>
+                    <div class="right"></div>
+                </div>
+            </div>
+            <div class="text"></div></div>
+        <div class="edit-control-bar">
+            <div class="autosave">
+                <label class="checkbox-sb">
+                    <input type="checkbox"  onclick="parent.builder.autosavePageData();"><span></span><em>Autosave</em>
+                </label>
+            </div>
+            <div class="edit-control-button">
+                <button class="save" type="button" onclick="parent.builder.save();
+                        return false;"><span>Save</span>
+                    <div class="clock">
+                        <div class="minutes-container"><div class="minutes"></div></div>
+                        <div class="seconds-container"><div class="seconds"></div></div>
+                    </div>
+                </button>
+                <button class="exit-btn" onclick="builder.exit();
+                        return false;" type="button">Exit</button>
+                <button class="screen-size settings" type="button" onclick="builder.menu.showGlobalSettings();
+                        return false;"></button>
+                <button class="screen-size pc active" onclick="parent.builder.toolbar.screenSize(this);
+                        return false;" type="button"></button>
+                <button class="screen-size tablet-vertical" onclick="parent.builder.toolbar.screenSize(this);
+                        return false;" type="button"></button>
+                <button class="screen-size phone-vertical" onclick="parent.builder.toolbar.screenSize(this);
+                        return false;" type="button"></button>
+                <button class="screen-size tablet-horizontal" onclick="parent.builder.toolbar.screenSize(this);
+                        return false;" type="button"></button>
+                <button class="screen-size phone-horizontal" onclick="parent.builder.toolbar.screenSize(this);
+                        return false;" type="button"></button>
+                <button class="arrow-btn hide-builder" onclick="parent.builder.toolbar.hideBuilder(this);
+                        return false;" type="button"></button>
+            </div>
+        </div>
+    </div>
+    <div id="builder-menu">
+        <div id="card">
+            <div class="card-wrap">
+                <div class="card-main">
+                    <div class="blocks-settings"></div>
+                    <div class="groups"></div>
+                    <div class="list-group"></div>
+                    <div class="global-settings"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="builder-content"><div id="builder-viewport" class="pc">
+        <iframe src="/?page_id=<?php echo $post_ID; ?>&qoob=true" scrolling="auto" name="builder-iframe" id="builder-iframe"></iframe>
+    </div></div>
+</div>
+<!--END BUILDER STRUCTURE-->
+<!--SCRIPT FOR BUILDER INIT-->
 <script type="text/javascript">
     var builder;
     jQuery(document).ready(function () {
@@ -35,10 +101,10 @@ require_once(ABSPATH . 'wp-admin/admin-header.php');
 </script>
 
 <div style="height: 1px; visibility: hidden; overflow: hidden;">
-    <?php
-    // fix missed meta boxes
-    require_once ABSPATH . 'wp-admin/edit-form-advanced.php';
-    ?>
+<?php
+// fix missed meta boxes
+require_once ABSPATH . 'wp-admin/edit-form-advanced.php';
+?>
 </div>
 <?php
 require_once(ABSPATH . 'wp-admin/admin-footer.php');
