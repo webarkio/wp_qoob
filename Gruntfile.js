32eb5f1cf1cd021358cefae1b0fd51444c33ec81
Maksym Pomazan <maxazan@gmail.com>
2016-06-30 08:35:02 +0000

    fix version

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: {
-            build: ['build/builder/*', 'build/builder/**', 'build/**']
+            build: []
         },
         compress: {
             // dev: {
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
                     '!package.json',
                     '!**/package.json'
                 ],
-                dest: 'qoob.wordpress/'
+                dest: 'wp_qoob/'
             }
         },
         shell: {
