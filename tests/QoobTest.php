9069e835d8ad5e2062d4658f1f62c5744470cc90
jjuja <bilouskoelena@gmail.com>
2016-08-31 14:22:55 +0300

    test for qoob.php

--- tests/QoobTest.php
+++ tests/QoobTest.php
@@ -1,8 +1,13 @@
 <?php
 
-class MockQoob extends Qoob {}
+class MockQoob extends Qoob {
+    public function __construct() {
+        $this->blocks_url = dirname(plugin_dir_path(__FILE__)).'/blocks';
+        $this->blocks_path = dirname(plugin_dir_path(__FILE__)).'/blocks';
+    }
+}
 
-class QoobTest extends WP_UnitTestCase {
+class QoobTest extends WP_UnitTestCase { //WP_UnitTestCase
 
     private $test;
 
@@ -11,8 +16,8 @@ class QoobTest extends WP_UnitTestCase {
         $this->test = null;
     }
 
-    public function testloadPageData() {
-        $module = new MockQoob();
+    public function testLoadPageData() {
+        $qoob = new MockQoob();
 
         //Setting current user to aministrator
         // $this->_setRole( 'administrator' );
@@ -31,58 +36,58 @@ class QoobTest extends WP_UnitTestCase {
         $data = [
             'page_id' => $post_id,
             'blocks' => [
-                'html' => 'Some text',
-                'data' => $testString
-                ]
-            ];
+                            'html' => 'Some text',
+                            'data' => file_get_contents('tests/demo_test.txt')
+                        ]
+                ];
 
         $data = json_encode($data);
 
         // $this->setExpectedException( 'WPAjaxDieContinueException' );
         // try {
-            $module->savePageData($data);
-            $newData = $module->loadPageData($post_id);
+            $qoob->savePageData($data);
+            $newData = $qoob->loadPageData($post_id);
         // } catch ( WPAjaxDieContinueException $e ) {}
 
-        $this->assertEquals($newData, $testString);
+        $this->assertEquals($newData, file_get_contents('tests/demo_test.txt'));
     }
 
     //???
-    public function testpluginUpdatee()
+    public function testPluginUpdate()
     {
-        $module = new MockQoob();
+        $qoob = new MockQoob();
         $ver_old = get_site_option('qoob_version');
-        $module->pluginUpdate();
+        $qoob->pluginUpdate();
         $ver_up = get_site_option('qoob_version');
         $this->assertEquals($ver_up, $ver_old);
     }
 
     //???
-    // public function testpluginUpdateTo_1_1_0()
+    // public function testPluginUpdateTo()
     // {
-    //     $module = new MockQoob();
-    //     $ver_last = $module->pluginUpdateTo_1_1_0();
+    //     $qoob = new MockQoob();
+    //     $ver_last = $qoob->pluginUpdateTo_1_1_0();
     //     $this->assertEquals($ver_last, '1.1.3');
     // }
 
 
     //testinfoMetabox()
 
-    public function testinfoMetaboxDisplay()
+    public function testInfoMetaboxDisplay()
     {
-        $module = new MockQoob();
+        $qoob = new MockQoob();
         $meta = '<p>Current page has been edited with Qoob Page Builder. To edit this page as regular one - go to Qoob editor by pressing "qoob it" button and remove all blocks.</p>';
 
-        $infometa = $module->infoMetaboxDisplay();
+        $infometa = $qoob->infoMetaboxDisplay();
         $metabox = ob_get_contents();
         ob_clean();
         $this->assertEquals($metabox, $meta);
     }
 
     //???
-    public function testsavePostMeta()
+    public function testSavePostMeta()
     {
-        $module = new MockQoob();
+        $qoob = new MockQoob();
 
         //Creating post in database
         $post_data = array(
@@ -90,7 +95,7 @@ class QoobTest extends WP_UnitTestCase {
         );
         $post_id = wp_insert_post( wp_slash($post_data) );
         add_post_meta($post_id, 'post_meta', 'Test fot SavePostMeta');
-        $module->savePostMeta($post_id);
+        $qoob->savePostMeta($post_id);
         $meta = get_post_meta($post_id);
         $this->assertEquals(array_key_exists('post_meta',$meta), true);
 
@@ -106,48 +111,49 @@ class QoobTest extends WP_UnitTestCase {
 
     // loadBlocksAssets()
 
-    public function testgetUrlAssets()
+    public function testGetUrlAssets()
     {
-        $module = new MockQoob();
-        $url = $module->getUrlAssets();
+
+        $qoob = new MockQoob();
+        $url = $qoob->getUrlAssets();
         $url_path = "http://example.org/wp-content/plugins/wp_qoob/assets/";
         $this->assertEquals($url, $url_path);
     }
 
-    public function testgetUrlQoob()
+    public function testGetUrlQoob()
     {
-        $module = new MockQoob();
-        $url = $module->getUrlQoob();
+        $qoob = new MockQoob();
+        $url = $qoob->getUrlQoob();
         $url_path = "http://example.org/wp-content/plugins/wp_qoob/qoob/";
         $this->assertEquals($url, $url_path);
     }
 
-    public function testgetPathTemplates()
+    public function testGetPathTemplates()
     {
-        $module = new MockQoob();
-        $template = $module->getPathTemplates();
+        $qoob = new MockQoob();
+        $template = $qoob->getPathTemplates();
         $this->assertEquals(!(strstr($template, 'wp_qoob/templates')), false);
     }
 
-    public function testsetDefaultTitle()
+    public function testSetDefaultTitle()
     {
-        $module = new MockQoob();
-        $title = $module->setDefaultTitle('Default');
+        $qoob = new MockQoob();
+        $title = $qoob->setDefaultTitle('Default');
         $this->assertEquals($title,'Default');
     }
 
-    public function testgetUrlPage()
+    public function testGetUrlPage()
     {
-        $module = new MockQoob();
-        $url = $module->getUrlPage('5');
+        $qoob = new MockQoob();
+        $url = $qoob->getUrlPage('5');
         $url_path = "http://example.org/wp-admin/post.php?post_id=5&post_type=&qoob=true";
         $this->assertEquals($url, $url_path);
     }
 
-    public function testaddEditLinkAction()
+    public function testAddEditLinkAction()
     {
-        $module = new MockQoob();
-        $url = $module->addEditLinkAction('edit');
+        $qoob = new MockQoob();
+        $url = $qoob->addEditLinkAction('edit');
         $url_path = "edit";
         $this->assertEquals($url, $url_path);
     }
@@ -156,9 +162,9 @@ class QoobTest extends WP_UnitTestCase {
 
     //public function testsetPost()
 
-    public function testshowButton()
+    public function testShowButton()
     {
-        $module = new MockQoob();
+        $qoob = new MockQoob();
 
         //Creating post in database
         $post_data = array(
@@ -177,7 +183,7 @@ class QoobTest extends WP_UnitTestCase {
 
         $data = json_encode($data);
 
-        $show = $module->showButton($post_id);
+        $show = $qoob->showButton($post_id);
         $this->assertEquals($show, false);
     }
 
@@ -187,14 +193,155 @@ class QoobTest extends WP_UnitTestCase {
 
     //renderPage()
 
-    public function testsetTitlePage()
+    public function testSetTitlePage()
     {
-        $module = new MockQoob();
-        $title = $module->setTitlePage();
+        $qoob = new MockQoob();
+        $title = $qoob->setTitlePage();
         $title_page = "Edit Page with qoob";
         $this->assertEquals($title, $title_page);
     }
 
+    //function iframeScripts()
+    //function frontendScripts()
+    //function adminScripts()
+    //function loadScripts()
 
+    public function testGetUrlItems()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass($qoob);
+        $method = $class->getMethod('getUrlItems');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob);//calls a function
+        $demo = array(
+                        'id' => 'demo',
+                        'url' =>dirname (plugin_dir_path(__FILE__)).'/blocks/demo/'
+                    );
+
+        $this->assertEquals($result[0], $demo);
+    }
+
+    public function testGetUrlQoobTemplates()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass($qoob);
+        $method = $class->getMethod('getUrlQoobTemplates');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob);//calls a function
+        $path =  dirname (plugin_dir_path(__FILE__))."/qoob/tmpl/block/block-default-blank.html";
+        $demo = array(
+                        'id' => 'block-default-blank.html',
+                        'url' => $path
+                    );
+
+        $this->assertEquals($result[0], $demo);
+    }
+
+    public function testGetItems()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass("MockQoob");
+        $method = $class->getMethod('getItems');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob);//calls a function
+        $this->assertEquals(array_key_exists('title',$result[0][defaults]), true);
+    }
+
+    public function testGetItem()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass("MockQoob");
+        $method = $class->getMethod('getItem');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob,'demo');//calls a function
+        $demo = array(
+                    'id' => 'demo',
+                    'url' =>dirname (plugin_dir_path(__FILE__)).'/blocks/demo/'
+                    );
+
+        $this->assertEquals($result, $demo);
+    }
+
+    public function testGetGroups()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass("MockQoob");
+        $method = $class->getMethod('getGroups');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob);//calls a function
+
+        $demo = array(
+                        'id' => 'demo',
+                        'label' => 'Demo',
+                        'position' => '0'
+                    );
+
+        $this->assertEquals($result[0], $demo);
+    }
+
+    //loadAssetsScripts()
+    //function loadData()
+
+    // public function testLoadData()
+    // {
+    //     $qoob = new MockQoob(); 
+    //     ob_start();
+    //     $qoob->loadData();
+    //     file_put_contents('test.txt', ob_get_contents());
+    //     ob_end_close();
+
+    //     // $data = ob_get_contents();
+    //     // $data = json_encode($data);
+    //     //print_r($metabox);
+    //     //ob_end_clean();
+ 
+    //     var_dump($data);
+    //     $this->assertEquals($data, 2);
+    // }
+
+    //function getTplFiles()
+
+    public function testGetHtml()
+    {
+        $qoob = new MockQoob();
+        $class  = new ReflectionClass("MockQoob");
+        $method = $class->getMethod('getHtml');
+        $method->setAccessible(true);//makes the property available
+        $result = $method->invoke($qoob,'demo');//calls a function
+        $html = '<div class="demo-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="header-block">{{title}}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>
+                    {{{text}}}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>';
+        $this->assertEquals($result, $html);
+    }
+    //function loadItem()
+
+    //function loadTmpl()
+    //function load_blocks_scripts($assets_type)
+
+    // public function testLoadBlocksScripts()
+    // {
+    //     $qoob = new MockQoob();
+    //     $blocks_path = dirname(get_template_directory()) . '/wp_qoob_theme/blocks/entypo';
+    //     $blocks_url = dirname(get_template_directory()) . '/wp_qoob_theme/blocks/entypo';
+    //     $theme_url = dirname(get_template_directory()) . '/wp_qoob_theme/blocks/entypo';
+    //     $config_json = dirname(get_template_directory()) . '/wp_qoob_theme/blocks/entypo/config.json';
+    //     $w = $qoob->load_blocks_scripts('style');
+    //     print_r($blocks_path);
+    //     print_r($blocks_url);
+    //     $this->assertEquals($w,2);
+    // }
 
 }
