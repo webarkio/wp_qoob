7cdc2d56c1af18037c4f026d0f0fad8afea61fe1
portapro <uvarenkov@gmail.com>
2017-04-25 15:39:59 +0300

    fix visible button "qoob"

--- assets/js/qoob-backend-custom.js
+++ assets/js/qoob-backend-custom.js
@@ -4,22 +4,28 @@
  */
 (function($) {
     $(document).ready(function() {
-        // Add button editor in WP admin
-        var postId = jQuery('#post_ID').val();
-        var url = 'post.php?post=' + postId + '&action=qoob';
-        var button = '<div class="cube-button-block"><div class="cube-button"><a href="' + url + '"><i class="cube"></i><span>' + qoob_backend_custom.button_text + '</span></a></div></div>';
-        $(button).insertAfter('div#titlediv');
+        if (typenow === 'page') {
+            // Add button editor in WP admin
+            var postId = jQuery('#post_ID').val();
+            var url = 'post.php?post=' + postId + '&action=qoob';
+            var button = '<div class="cube-button-block"><div class="cube-button"><a href="' + url + '"><i class="cube"></i><span>' + qoob_backend_custom.button_text + '</span></a></div></div>';
+            $(button).insertAfter('div#titlediv');
+        }
 
         // page "qoob-manage-libs"
         var form = $('#qoob-filters'),
             filters = form.find('.qoob-filters');
         filters.hide();
         form.find('input:radio').change(function() {
-        	filters.slideUp('fast');
-			switch ( $(this).val() ) {
-				case 'url': $('#qoob-url').slideDown(); break;
-				case 'file': $('#qoob-file').slideDown(); break;
-			}
+            filters.slideUp('fast');
+            switch ($(this).val()) {
+                case 'url':
+                    $('#qoob-url').slideDown();
+                    break;
+                case 'file':
+                    $('#qoob-file').slideDown();
+                    break;
+            }
         });
         $('#qoob-url').slideDown();
     });
